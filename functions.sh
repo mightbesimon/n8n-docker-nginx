429804c65fadbeb270b7963e81a46f5a0a1c4aaa
mightbesimon <60593422+mightbesimon@users.noreply.github.com>
2026-02-06 13:39:50 +0100

    add logging, improve show status and pull updates

--- functions.sh
+++ functions.sh
@@ -14,13 +14,32 @@ function reload_vector
 	cp $HOME/n8n-docker-nginx/vector.yaml /etc/vector/
 	systemctl restart vector
 }
-function n8n_logs
+function show_logs
 {
-	docker compose -f $HOME/n8n-docker-nginx/docker-compose.yml logs n8n
+	docker compose -f $HOME/n8n-docker-nginx/docker-compose.yml logs n8n --tail 100
 }
-function status
+function show_status
 {
-	systemctl status docker nginx vector
-	curl -I http://localhost:5678
-	docker compose -f $HOME/n8n-docker-nginx/docker-compose.yml ps
+	systemctl is-active nginx > /dev/null \
+	&& log info  'nginx is active' \
+	|| log error 'nginx is inactive'
+	docker compose -f $HOME/n8n-docker-nginx/docker-compose.yml ps | grep n8n | grep Up > /dev/null \
+	&& log info  'n8n container is up' \
+	|| log error 'n8n container is down'
+	curl -I http://localhost:5678/healthz > /dev/null \
+	&& log info  'n8n is reachable' \
+	|| log error 'n8n is unreachable'
+	systemctl is-active vector > /dev/null \
+	&& log info  'metrics is active' \
+	|| log error 'metrics is inactive'
+}
+function pull
+{
+	git -C $HOME/profile.bash/ pull --rebase \
+	&& log info  'profile repo updated' \
+	|| log error 'profile repo failed to pull'
+	git -C $HOME/n8n-docker-nginx/ pull --rebase \
+	&& log info  'n8n repo updated' \
+	|| log error 'n8n repo failed to pull'
+	source $HOME/.profile
 }
